a7d0687c6f57eeecae5c0656b08bbe3b44be07a6
0J3 <0J3@wafflepowered.com>
2021-06-04 18:20:37 +0200

    update config and push wip build js file

--- build.js
+++ build.js
@@ -22,13 +22,59 @@ if (
 // Promise is returned
 builder
 	.build({
-		targets: Platform.WINDOWS.createTarget('nsis'),
+		targets: [
+			Platform.WINDOWS.createTarget('nsis'),
+			Platform.LINUX.createTarget(),
+		],
 		config: {
 			appId: 'lgbt.nora.QuickRPC',
 			productName: 'QuickRPC',
 			copyright: 'Copyright © 2021 0J3.',
+			files: [
+				'**/*',
+				'!**/node_modules/*/{CHANGELOG.md,README.md,README,readme.md,readme}',
+				'!**/node_modules/*/{test,__tests__,tests,powered-test,example,examples}',
+				'!**/node_modules/*.d.ts',
+				'!**/node_modules/.bin',
+				'!**/*.{iml,o,hprof,orig,pyc,pyo,rbc,swp,csproj,sln,xproj}',
+				'!.editorconfig',
+				'!**/._*',
+				'!**/{.DS_Store,.git,.hg,.svn,CVS,RCS,SCCS,.gitignore,.gitattributes}',
+				'!**/{__pycache__,thumbs.db,.flowconfig,.idea,.vs,.nyc_output}',
+				'!**/{appveyor.yml,.travis.yml,circle.yml}',
+				'!**/{npm-debug.log,yarn.lock,.yarn-integrity,.yarn-metadata.json}',
+				'!**/key',
+				'!**/key/*',
+				'!**/key/*.pfx',
+				'!**/key/cert.pfx',
+				'!key/cert.pfx',
+			],
 
 			...certData,
+		},
+		win: {
+			publisherName: '0J3',
+			compression: 'maximum',
+		},
+		linux: {
+			synopsis: 'A Discord RPC Client',
+			description: 'A Discord Rich Presence Client',
+			executableName: 'QuickRPC',
+			category: 'Network',
+			target: ['snap', 'deb', 'rpm', 'apk'],
+			compression: 'maximum',
+		},
+		deb: {
+			depends: [
+				'gconf2',
+				'gconf-service',
+				'libnotify4',
+				'libappindicator1',
+				'libxtst6',
+				'libnss3',
+				'discord',
+			],
+			priority: 'standard',
 		},
 	})
 	.then(v => {
